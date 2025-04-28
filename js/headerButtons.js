2fb1c28093ccd19543b74d7acfbb0882b132eab5
MattiaBenincasa <mattia.benincasa03@gmail.com>
2025-04-28 15:25:25 +0200

    Added responsive behaviour to the footer. Changes in offcanvas menu

--- js/headerButtons.js
+++ js/headerButtons.js
@@ -6,15 +6,23 @@ window.addEventListener('resize', function (){
         const menuButton = document.getElementById('menu-button');
         const socialBar = document.getElementById('social-bar')
         const logoBar = document.getElementById('logo-bar');
+        const videoSection = document.getElementById('video-container');
+        const body = document.getElementById("main-container");
+        const inEvidenza = document.getElementById("in-evidenza");
         socialBar.appendChild(subButton)
         logoBar.insertBefore(menuButton, logoBar.firstChild);
+        body.insertBefore(videoSection, inEvidenza);
     }else{
         const subButton = document.getElementById('sub-button');
         const menuButton = document.getElementById('menu-button');
         const accountBar = document.getElementById('account-bar');
         const primaryNavList = document.getElementById('primary-nav-list');
+        const videoSection = document.getElementById('video-container');
+        const body = document.getElementById("main-container");
+        const primoPiano = document.getElementById("primo-piano");
         accountBar.appendChild(subButton);
         primaryNavList.insertBefore(menuButton, primaryNavList.firstChild);
+        body.insertBefore(videoSection, primoPiano);
     }
 })
 
--- js/sidenav.js
+++ js/sidenav.js
@@ -2,6 +2,7 @@
 function openNav() {
     const sidebar = document.getElementById('sidenav');
     document.getElementById('overlay').style.display = "block";
+    sidebar.style.padding = '16px';
 
     if (window.innerWidth < 900) {
         sidebar.style.width = "100%";
@@ -13,7 +14,9 @@ function openNav() {
 
 function closeNav() {
     document.getElementById('sidenav').style.width = "0";
+    document.getElementById('sidenav').style.padding = "0";
     document.getElementById('overlay').style.display = "none";
+
 }
 
 document.getElementById('overlay').addEventListener("click", closeNav)
